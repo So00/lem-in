6d40474b499990ce3a5078f376597ad9c69b9d04
Anthony TOURNERIE <atourner@e2r5p2.42.fr>
2018-03-08 14:26:54 +0100

    sorted test

--- test/error.sh
+++ test/error.sh
@@ -1,6 +1,6 @@
 	#!/bin/bash
 	rm -rf error.txt
-	list_file=`find . -name "map*" | cut -d '/' -f 2`
+	list_file=`find . -name "map*"`
 	for file in $list_file
 	do
 		./lem-in < $file | ./error $file
